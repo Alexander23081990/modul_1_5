34d979dfb10f08c3e87bc7195a5c7eb7c3fe12bc
Alexander23081990 <95206213+Alexander23081990@users.noreply.github.com>
2024-09-20 14:40:03 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 4, 'Helo', 10, 'apple', True
+print(immutable_var)
+#  immutable_var[0] = 60 - элементы кортежа не изменяемы
+#  print(immutable_var)
+mutable_list = [3, 'buy', 20, 'peach', False]
+print(mutable_list)
+mutable_list[1] = 'welcome'
+print(mutable_list)
